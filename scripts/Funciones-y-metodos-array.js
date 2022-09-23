ab7c9f0200b4f609ec66bc87db4c71549251575b
arely montoro cecilio <sulemaliotmail.com>
2022-09-23 14:07:11 -0500

    tarea actualizada

---
+++ scripts/Funciones-y-metodos-array.js
@@ -0,0 +1,50 @@
+const main = () => {
+//Festival de música 
+/*const yearborn = (age) => {
+    for (i=0; i<=2004; i++);
+}
+const isApproved = (yearborn) => {
+    if(yearborn<=2004){
+        console.log ("Es mayor de edad, puede pasar, bienvenido");
+    }else {
+        console.log("No es mayor de edad, puede pasar, a retirarse :v");
+    }
+}
+isApproved(2004);*/
+
+//Funcion que sea un discriminante para ecuaciones
+ /*function suma(a,b,c){
+        const result =b**2-4*a*c
+        return result
+    }
+const discriminante= (suma) => {
+        if(suma>0){
+            console.log("Tiene dos soluciones");
+        }
+    
+        if (suma<0){
+            console.log("No tiene solucion");
+        }   
+        if (suma===0){
+            console.log("Tiene una solucion");
+        }
+    }
+//Los valores de la funcion se meten aquí abajo con el orden a,b,c
+const res = suma(8,15,18);
+console.log (`El resultado es ${res}`);
+discriminante(res); */
+
+//Array. push 
+movies =[];
+movies.push ("Sucker Punch");
+movies.push ("Scott Pilgrim vs the world");
+movies.push ("The Baudelaire");
+movies.push ("The invisible man");
+movies.push ("La niña de la capa roja");
+movies.push ("Suicide Room");
+movies.push ("Alice in wonderland 1 y 2 ");
+movies.push ("Corpse Bride");
+console.log(movies);
+}
+main();
+
--- scripts/funciones.js
+++ scripts/funciones.js
@@ -1,10 +1,8 @@
-//Saludo por función 
-const nombre = "Arely Montoro Cecilio"
-const age = 22
-function saludo(){
-    console.log(`Hola mi nombre es ${nombre} y tengo ${age} años`);
+//Saludo por funció
+const saludo =(name,age) =>{
+    console.log(`Hola mi nombre es ${name} y tengo ${age} años`);
 }
-saludo()
+saludo("Arely",22)
 
 console.log(`Función para encontrar el producto de dos numeros y dividirlo por uno tercero `)
 const operation = function (a,b,c){
@@ -15,11 +13,9 @@ console.log(operation(12,4,8));
 //Función que cuenta hasta el numero asignado 
 console.log(`Función que cuenta hasta cierto número`)
 
-function counting() {
-    let number =1, asigment=16;
-    while(number<=asigment){
-        console.log(number);
-        number++
+const counter = (number) => {
+    for (i=1;i<=number;i++){
+        console.log(i);
     }
 }
-counting()
+counter(6);
